f936fb91e9fa41e4aa245e10a2da111c5feb555d
snoopyathome <mail-an-muehlbauer@web.de>
2021-01-10 17:28:30 +0100

    Update deploy.sh

--- files/deploy.sh
+++ files/deploy.sh
@@ -2,10 +2,9 @@
 #
 cd /usr/local/tomcat/webapps/
 rm -r ROOT docs examples host-manager manager
-export VERSION="0.9.9"
+export VERSION="1.3.0"
 export DESTINATION="/opt/guacamole"
 mkdir -p "$DESTINATION"
-echo "Downloading JDBC auth version $VERSION ..."
 curl -L "http://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-jdbc-$VERSION.tar.gz/download" | \
 tar -zx \
  -C "$DESTINATION" \
@@ -15,11 +14,8 @@ tar -zx \
  "*.jar" \
  "*.sql"
 export DESTINATION="/usr/local/tomcat/webapps"
-echo "Downloading Guacamole version $VERSION to $DESTINATION ..."
 curl -L "http://sourceforge.net/projects/guacamole/files/current/binary/guacamole-${VERSION}.war/download" > "$DESTINATION/ROOT.war"
-
 export DESTINATION="/opt/guacamole"
-echo "Downloading PostgreSQL JDBC driver ..."
 mkdir -p "$DESTINATION/postgresql/"
 curl -L "https://jdbc.postgresql.org/download/postgresql-9.4-1201.jdbc41.jar" > "$DESTINATION/postgresql/postgresql-9.4-1201.jdbc41.jar"
 apt-get update && apt-get install -y postgresql-client
